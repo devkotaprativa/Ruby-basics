31e2d5e83e52c35011dc20fb15f858bfa3b03b91
prativa <devkotaprativa@gmail.com>
2015-09-30 13:47:43 +0545

    Decryptor Decryptor class added which inherits from encryptor class. It includes methods decrypt_letter, decrypt

--- encryptor.rb
+++ encryptor.rb
@@ -19,11 +19,6 @@ class Encryptor
 		letters = string.split("")
   	
   	# 2. Encrypt those letters one at a time, gathering the results
-  	#  results = []
-  	# letters.each do |i|
-   #  encrypted_letter = encrypt_letter(i)
-   #  results.push(encrypted_letter)
-  	# end
   	results = letters.collect do |i|
   		encrypt_letter(i)
   		
@@ -33,8 +28,26 @@ class Encryptor
 		results.join
 
 	end
-
-
 end
+
+class Decryptor < Encryptor
+	def decrypt_letter(letter)
+  lowercase_letter = letter.downcase
+  cipher[lowercase_letter]
+	end
+
+	def decrypt(string)
+		letters = string.split("")
+  	results = letters.collect do |i|
+  		decrypt_letter(i)
+  		
+  	end
+		results.join
+	end
+end
+
 e = Encryptor.new
-puts e.encrypt("hello")
+puts e.encrypt("hello World")
+
+d = Decryptor.new
+puts d.decrypt("uryybjbeyq")
